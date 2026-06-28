b306756070c9f9975859c43a7da6442e6ea8a0bd
almissbah110-glitch <almissbah110@gmail.com>
2026-06-28 11:27:57 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,19 @@
+<?php
+
+		$fullname = $_POST['fullname'];
+		$email = $_POST['email']; 
+		$number = $_POST['number'];
+		$subject = $_POST['subject']; 
+		$message = $_POST['message'];
+		$send = "joizwyjvaxaj@almissbah.org";
+		$msg .="Name : ".$fullname."\n";
+		$msg .="E-mail : ".$email."\n";
+		$msg .="Phone : ".$number."\n";
+		$msg .="subject : ".$subject."\n";
+		$msg .="message : \n".$message."\n";
+		$header = 'Extra-theme' . "\r\n";
+		mail($send,$subject,$msg,$header);
+				
+	echo "<center> <br><h1>تم ارسال الرسالة بنجاح</h1></center>";
+	header("refresh:1; url=contact.html");
+
---
+++ upload.php
@@ -0,0 +1,25 @@
+<?php
+
+		$filename = $_FILES['file']['name'];
+		$filesize = $_FILES['file']['size'];
+		$tmpname = $_FILES['file']['tmp_name'];
+		$filetype = $_FILES['file']['type'];
+		$folder = "uploads/";
+		$maxfile = "1024000";
+		$typefile = array('application/msword','application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+		$micro = microtime();
+
+		if(isset($_POST['submit'])){
+			if (empty($filename)) {
+				echo '<h1><br /><center>يرجى تحديد الملف ';
+			}else if ($filesize > $maxfile) {
+				echo "<h1><br /><center>حجم الملف كبير جدا ";
+			}else if (!in_array($filetype,$typefile)){
+				echo "<h1><br /><center>الملف غير مسموح ";
+			}else {
+				echo "<h1><br /><center>تم ارسال الملف بنجاح </center>";
+				move_uploaded_file($tmpname,$folder.$micro.$filename);
+			}
+			header("refresh:1; url=contact.html");
+		}
+?>
